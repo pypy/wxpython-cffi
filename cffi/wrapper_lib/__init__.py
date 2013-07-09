333c8a78f3e46cf2145a7de8ca1797c30f560f70
Tyler Wade <wayedt@gmail.com>
2013-07-08 19:01:27 -0500

    Update wrapper_lib to match mockup development

--- cffi/wrapper_lib/__init__.py
+++ cffi/wrapper_lib/__init__.py
@@ -3,3 +3,14 @@ from cppwrapper import (
     take_ownership, give_ownership)
 from multimethod import (
     Multimethod, StaticMultimethod, ClassMultimethod, register_type)
+
+classname_registry = {}
+
+def register_classname(name, cls):
+    if name in classname_registry:
+        raise KeyError("Class %s is already registered to '%s'" %
+                       (classname_registry[name], name))
+    classname_registry[name] = cls
+
+def class_from_classname(name):
+    return classname_registry[name]
