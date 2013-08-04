d979ae1489867070e5b671a8c6c66f20bf657b2d
Tyler Wade <wayedt@gmail.com>
2013-08-04 14:34:00 -0500

    Remove redundant multimethod tests

--- cffi/wrapper_lib/test/test_multimethod.py
+++ cffi/wrapper_lib/test/test_multimethod.py
@@ -50,85 +50,68 @@ class Number(object):
             return Number(obj)
 
 class ClassWithMMs(object):
-    positional_builtins = Multimethod()
+    builtin_types = Multimethod()
 
-    @positional_builtins.overload()
-    def positional_builtins(self):
+    @builtin_types.overload()
+    def builtin_types(self):
         return tuple()
 
-    @positional_builtins.overload(i=int)
-    def positional_builtins(self, i):
+    @builtin_types.overload(i=int)
+    def builtin_types(self, i):
         return (i,)
 
-    @positional_builtins.overload(i=int, l=list)
-    def positional_builtins(self, i, l):
+    @builtin_types.overload(i=int, l=list)
+    def builtin_types(self, i, l):
         return (i, l)
 
-    keyword_builtins = Multimethod()
-
-    @keyword_builtins.overload(i=int, l=list)
-    def keyword_builtins(self, i=10, l=[1, 2, 3]):
-        return (i, l)
-
-    @keyword_builtins.overload(i=float, l=tuple)
-    def keyword_builtins(self, i=0.2, l=(1, 2, 3)):
-        return (i, l)
-
-
-    mixed_builtins = Multimethod()
-
-    @mixed_builtins.overload(i=int, l=list)
-    def mixed_builtins(self, i, l=[1, 2, 3]):
-        return (i, l)
-
-    @mixed_builtins.overload(f=float, l=tuple)
-    def mixed_builtins(self, f, l=(1, 2, 3)):
-        return (f, l)
-
-    @mixed_builtins.overload(one=tuple, two=tuple)
-    def mixed_builtins(self, one=(1, 2), two=(2, 3)):
+    @builtin_types.overload(one=tuple, two=tuple)
+    def builtin_types(self, one, two=(2, 3)):
         return one + two
 
-    instance_builtins = Multimethod()
+    @builtin_types.overload(f=float, k=tuple)
+    def builtin_types(self, f, k=(1, 2, 3)):
+        return (f, k)
 
-    @instance_builtins.overload()
-    def instance_builtins(self):
+    instance_mm = Multimethod()
+
+    @instance_mm.overload()
+    def instance_mm(self):
         return (self,)
 
-    @instance_builtins.overload(d=dict)
-    def instance_builtins(self, d):
+    @instance_mm.overload(d=dict)
+    def instance_mm(self, d):
         return (self, d)
 
-    @instance_builtins.overload(i=int, n=int)
-    def instance_builtins(self, i=9, n=10):
+    @instance_mm.overload(i=int, n=int)
+    def instance_mm(self, i, n=10):
         return (self, i, n)
 
-    class_builtins = ClassMultimethod()
+    class_mm = ClassMultimethod()
 
-    @class_builtins.overload()
-    def class_builtins(cls):
+    @class_mm.overload()
+    def class_mm(cls):
         return (cls,)
 
-    @class_builtins.overload(d=dict)
-    def class_builtins(cls, d):
+    @class_mm.overload(d=dict)
+    def class_mm(cls, d):
         return (cls, d)
 
-    @class_builtins.overload(i=int, n=int)
-    def class_builtins(cls, i=9, n=10):
+    @class_mm.overload(i=int, n=int)
+    def class_mm(cls, i=9, n=10):
         return (cls, i, n)
 
-    static_builtins = StaticMultimethod()
+    static_mm = StaticMultimethod()
 
-    @static_builtins.overload()
-    def static_builtins():
+    @static_mm.overload()
+    def static_mm():
         return tuple()
 
-    @static_builtins.overload(d=dict)
-    def static_builtins(d):
+    @static_mm.overload(d=dict)
+    def static_mm(d):
         return (d,)
 
-    @static_builtins.overload(i=int, n=int)
-    def static_builtins(i=9, n=10):
+    @static_mm.overload(i=int, n=int)
+    def static_mm(i=9, n=10):
         return (i, n)
 
     usertypes = Multimethod()
@@ -162,65 +145,55 @@ ClassWithMMs.outofbody.finish()
 class TestMultimethods(object):
     def test_positional(self):
         mm_obj = ClassWithMMs()
-        assert mm_obj.positional_builtins() == tuple()
-        assert mm_obj.positional_builtins(42) == (42,)
-        assert mm_obj.positional_builtins(-1, [1]) == (-1, [1])
+        assert mm_obj.builtin_types() == tuple()
+        assert mm_obj.builtin_types(42) == (42,)
+        assert mm_obj.builtin_types(-1, [1]) == (-1, [1])
+        assert mm_obj.builtin_types(l=[1], i=-1) == (-1, [1])
+
+        assert mm_obj.builtin_types((0,)) == (0, 2, 3)
+        assert mm_obj.builtin_types((2,), (1,)) == (2, 1)
+        assert mm_obj.builtin_types((2,), two=(1,)) == (2, 1)
+        assert mm_obj.builtin_types(one=(2,), two=(1,)) == (2, 1)
+        assert mm_obj.builtin_types(two=(2,), one=(1,)) == (1, 2)
+
+        assert mm_obj.builtin_types(2.0) == (2.0, (1, 2, 3))
+        assert mm_obj.builtin_types(k=(0, 1), f=0.2) == (0.2, (0, 1))
 
         with pytest.raises(TypeError):
-            mm_obj.positional_builtins("foo")
+            mm_obj.builtin_types("foo")
 
-    def test_keyword(self):
-        mm_obj = ClassWithMMs()
-        assert mm_obj.keyword_builtins(l=[-1, 4]) == (10, [-1, 4])
-        assert mm_obj.keyword_builtins(i=99) == (99, [1, 2, 3])
-        assert mm_obj.keyword_builtins(i=99, l=[-1, 4]) == (99, [-1, 4])
+        with pytest.raises(TypeError):
+            mm_obj.builtin_types((1,), (2,), (3,))
 
-        assert mm_obj.keyword_builtins(l=(-1, 4)) == (0.2, (-1, 4))
-        assert mm_obj.keyword_builtins(i=3.1) == (3.1, (1, 2, 3))
-        assert mm_obj.keyword_builtins(i=3.1, l=(-1, 4)) == (3.1, (-1, 4))
-
-    def test_mixed(self):
-        mm_obj = ClassWithMMs()
-        assert mm_obj.mixed_builtins(0) == (0, [1, 2, 3])
-        assert mm_obj.mixed_builtins(0, []) == (0, [])
-        assert mm_obj.mixed_builtins(0, l=[]) == (0, [])
-
-        assert mm_obj.mixed_builtins(.7) == (.7, (1, 2, 3))
-        assert mm_obj.mixed_builtins(.7, l=tuple()) == (.7, tuple())
-
-        assert mm_obj.mixed_builtins() == (1, 2, 2, 3)
-        assert mm_obj.mixed_builtins((0,)) == (0, 2, 3)
-        assert mm_obj.mixed_builtins((2,), (1,)) == (2, 1)
-        assert mm_obj.mixed_builtins((2,), two=(1,)) == (2, 1)
-        assert mm_obj.mixed_builtins(one=(2,), two=(1,)) == (2, 1)
-        assert mm_obj.mixed_builtins(two=(2,), one=(1,)) == (1, 2)
+        with pytest.raises(TypeError):
+            mm_obj.builtin_types((1, 2), 0.1)
 
     def test_instace_methods(self):
         mm_cls = ClassWithMMs
         mm_obj = ClassWithMMs()
-        assert mm_obj.instance_builtins() == (mm_obj,)
-        assert mm_obj.instance_builtins({1: 10}) == (mm_obj, {1: 10})
-        assert mm_obj.instance_builtins(n=2, i=3) == (mm_obj, 3, 2)
+        assert mm_obj.instance_mm() == (mm_obj,)
+        assert mm_obj.instance_mm({1: 10}) == (mm_obj, {1: 10})
+        assert mm_obj.instance_mm(n=2, i=3) == (mm_obj, 3, 2)
 
-        assert mm_obj.instance_builtins() == mm_cls.instance_builtins(mm_obj)
-        assert (mm_obj.instance_builtins({1: 10}) ==
-                mm_cls.instance_builtins(mm_obj, {1: 10}))
-        assert (mm_obj.instance_builtins(n=2, i=3) ==
-                mm_cls.instance_builtins(mm_obj, n=2, i=3))
+        assert mm_obj.instance_mm() == mm_cls.instance_mm(mm_obj)
+        assert (mm_obj.instance_mm({1: 10}) ==
+                mm_cls.instance_mm(mm_obj, {1: 10}))
+        assert (mm_obj.instance_mm(n=2, i=3) ==
+                mm_cls.instance_mm(mm_obj, n=2, i=3))
 
     def test_class_methods(self):
         mm_cls = ClassWithMMs
         mm_obj = ClassWithMMs()
-        assert mm_obj.class_builtins() == mm_cls.class_builtins()
-        assert mm_obj.class_builtins({1: 10}) == mm_obj.class_builtins({1: 10})
-        assert mm_obj.class_builtins(n=2, i=3) == mm_obj.class_builtins(n=2, i=3)
+        assert mm_obj.class_mm() == mm_cls.class_mm()
+        assert mm_obj.class_mm({1: 10}) == mm_obj.class_mm({1: 10})
+        assert mm_obj.class_mm(n=2, i=3) == mm_obj.class_mm(n=2, i=3)
 
     def test_static_methods(self):
         mm_cls = ClassWithMMs
         mm_obj = ClassWithMMs()
-        assert mm_obj.static_builtins() == mm_cls.static_builtins()
-        assert mm_obj.static_builtins({1: 10}) == mm_obj.static_builtins({1: 10})
-        assert mm_obj.static_builtins(n=2, i=3) == mm_obj.static_builtins(n=2, i=3)
+        assert mm_obj.static_mm() == mm_cls.static_mm()
+        assert mm_obj.static_mm({1: 10}) == mm_obj.static_mm({1: 10})
+        assert mm_obj.static_mm(n=2, i=3) == mm_obj.static_mm(n=2, i=3)
 
     def test_usertypes(self):
         mm_obj = ClassWithMMs()
