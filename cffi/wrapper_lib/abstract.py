b900d12f9889d613586c7bf31129dc474dd60a43
Tyler Wade <wayedt@gmail.com>
2013-09-24 15:33:37 -0500

    Fix classes that derive from class with pure virtual methods

--- cffi/wrapper_lib/abstract.py
+++ cffi/wrapper_lib/abstract.py
@@ -6,43 +6,23 @@ from cppwrapper import CppWrapper
 def abstract__new__(cls, *args, **kwargs):
     raise TypeError('%s cannot be instantiated or sub-classed' % cls.__name__)
 
-@classmethod
 def purevirtual__new__(cls, *args, **kwargs):
-    raise TypeError("%s represents a C++ abstract class and cannot be "
-                    "instantiated" % cls.__name__)
+    if cls is kwargs['_baseclass']:
+        raise TypeError("%s represents a C++ abstract class and cannot be "
+                        "instantiated" % cls.__name__)
+    else:
+        del kwargs['_baseclass']
+        return CppWrapper.__new__(cls, *args, **kwargs)
 
 def abstract_class(cls):
-    '''
-    @staticmethod
-    def __new__(cls, *args, **kwargs):
-        if '_override_abstract_class' in kwargs:
-            return super(base_class, cls).__new__(cls, *args, **kwargs)
-    base_class.__new__ = __new__
-    return base_class
-    '''
     cls.__new__ = abstract__new__
     return cls
 
 def concrete_subclass(cls):
-    '''
-    @staticmethod
-    def __new__(cls, *args, **kwargs):
-        kwargs['_override_abstract_class'] = True
-        return super(base_class, cls).__new__(cls, *args, **kwargs)
-    base_class.__new__ = __new__
-    return base_class
-    '''
     cls.__new__ = staticmethod(CppWrapper.__new__)
     return cls
 
 def purevirtual_abstract_class(cls):
-    '''
-    @staticmethod
-    def __new__(cls, *args, **kwargs):
-        if cls is base_class:
-        return super(base_class, cls).__new__(cls, *args, **kwargs)
-    base_class.__new__ = __new__
-    return base_class
-    '''
-    cls.__new__ = purevirtual__new__
+    cls.__new__ = functools.partial(
+        purevirtual__new__, _baseclass=cls)
     return cls
