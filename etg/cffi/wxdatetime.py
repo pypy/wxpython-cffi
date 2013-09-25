991ac2edd9ac6e82a4a0aab9b08553cde17256ab
Tyler Wade <wayedt@gmail.com>
2013-09-24 22:13:23 -0500

    Fixing time_t for Windows broke it elsewhere. Fix it for OS X and Linux
    Hopefully, this still works on Windows

--- etg/cffi/wxdatetime.py
+++ etg/cffi/wxdatetime.py
@@ -4,4 +4,5 @@ import sys
 def run(module):
     if sys.platform == 'win32':
         module.cdefs_cffi.append('typedef long long time_t;')
+        module.find('time_t').ignore()
     module.addItem(etgtools.DefineDef(name='wxLOCALE_LOAD_DEFAULT', value=''))
