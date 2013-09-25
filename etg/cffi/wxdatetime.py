576cffca5c9530e537d8e8e21e3aa61f5058d4ca
Matti Picus <matti.picus@gmail.com>
2013-09-25 13:40:10 +0300

    fix

--- etg/cffi/wxdatetime.py
+++ etg/cffi/wxdatetime.py
@@ -4,4 +4,6 @@ import sys
 def run(module):
     if sys.platform == 'win32':
         module.cdefs_cffi.append('typedef long long time_t;')
+    else:
+        module.cdefs_cffi.append('typedef long time_t;')
     module.addItem(etgtools.DefineDef(name='wxLOCALE_LOAD_DEFAULT', value=''))
