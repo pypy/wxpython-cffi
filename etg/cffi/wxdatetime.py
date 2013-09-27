52442e491c87af138daca218b91f1a13926f59e7
Tyler Wade <wayedt@gmail.com>
2013-09-27 10:54:17 -0500

    Backout 243:8929f1f9df31

--- etg/cffi/wxdatetime.py
+++ etg/cffi/wxdatetime.py
@@ -2,4 +2,9 @@ import etgtools
 import sys
 
 def run(module):
+    if sys.platform == 'win32':
+        module.cdefs_cffi.append('typedef long long time_t;')
+        module.find('time_t').ignore()
+    else:
+        module.cdefs_cffi.append('typedef long time_t;')
     module.addItem(etgtools.DefineDef(name='wxLOCALE_LOAD_DEFAULT', value=''))
