4cf69242a50e5bf1e65048c536d685b0a0fcad1a
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2015-07-27 13:31:22 +0200

    Add _adv module. Completely empty for now

--- etg/_adv.py
+++ etg/_adv.py
@@ -25,30 +25,30 @@ ITEMS  = [ ]
 # included as part of this module. These should all be items that are put in
 # the wxWidgets "adv" library in a multi-lib build.
 INCLUDES = [  
-             'aboutdlg',
-             'helpext',
-             'commandlinkbutton',
-             'dateevt',
-             'datectrl',
-             'calctrl',
-             'hyperlink',
-             'tipdlg',
-             'taskbar',
-             'sound',
-             'joystick',
-             'animate',
-             'bannerwindow',
-             'editlbox',
-             'notifmsg',
-             'splash',
-             'sashwin',
-             'laywin',
-             'odcombo',
-             'bmpcbox',
-             'richtooltip',
-             'timectrl',
-             'treelist',
-             'wizard',
+             #'aboutdlg',
+             #'helpext',
+             #'commandlinkbutton',
+             #'dateevt',
+             #'datectrl',
+             #'calctrl',
+             #'hyperlink',
+             #'tipdlg',
+             #'taskbar',
+             #'sound',
+             #'joystick',
+             #'animate',
+             #'bannerwindow',
+             #'editlbox',
+             #'notifmsg',
+             #'splash',
+             #'sashwin',
+             #'laywin',
+             #'odcombo',
+             #'bmpcbox',
+             #'richtooltip',
+             #'timectrl',
+             #'treelist',
+             #'wizard',
              ]
 
 
@@ -72,7 +72,8 @@ def run():
     # Tweak the parsed meta objects in the module object as needed for
     # customizing the generated code and docstrings.
     
-    module.addHeaderCode('#include <wxpy_api.h>')
+    # XXX(amauryfa): only for SIP?
+    # module.addHeaderCode('#include <wxpy_api.h>')
     module.addImport('_core')
     module.addPyCode("import wx", order=10)
     module.addInclude(INCLUDES)
